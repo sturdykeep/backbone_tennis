d36db4cb3562fc6ef9a7376a0b18d4d7dc106228
DevOwl <christian.muehle@devowl.de>
2023-02-05 10:04:29 +0100

    collision handling for the ball

--- lib/nodes/dash_ball_node.dart
+++ lib/nodes/dash_ball_node.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:backbone/position_node.dart';
 import 'package:backbone_tennis/game_consts.dart';
 import 'package:backbone_tennis/messages/player_scored_message.dart';
+import 'package:backbone_tennis/nodes/player_node.dart';
 import 'package:backbone_tennis/traits/ball_trait.dart';
 import 'package:backbone_tennis/traits/sprite_trait.dart';
 import 'package:flame/collisions.dart';
@@ -45,7 +46,8 @@ class DashBallNode extends PositionNode with CollisionCallbacks {
   void onCollisionStart(
       Set<Vector2> intersectionPoints, PositionComponent other) {
     super.onCollisionStart(intersectionPoints, other);
-
+    final firstCollisionPoint = intersectionPoints.first;
+    final ballTrait = get<BallTrait>();
     // If we hit the left/right a player scores, if we hit top or bottom we reflect off
     if (other is ScreenHitbox) {
       if (intersectionPoints.first.x == 0) {
@@ -55,12 +57,26 @@ class DashBallNode extends PositionNode with CollisionCallbacks {
         realm!.pushMessage(PlayerScoredMessage(playerSide: PlayerSide.left));
         return;
       }
+      // At this point we know that the ball touched the top/bottom of the screen
+      ballTrait.direction.multiply(Vector2(1, -1));
+    } else {
+      // Ball touched the player
+      final playerRect = (other as PlayerNode).toAbsoluteRect();
+      final isLeftHit = firstCollisionPoint.x == playerRect.left;
+      final isRightHit = firstCollisionPoint.x == playerRect.right;
+      final isTopHit = firstCollisionPoint.y == playerRect.bottom;
+      final isBottomHit = firstCollisionPoint.y == playerRect.top;
+
+      final isLeftOrRight = isLeftHit || isRightHit;
+      final isTopOrBottom = isTopHit || isBottomHit;
+
+      if (isLeftOrRight) {
+        ballTrait.direction.multiply(Vector2(-1, 1));
+      }
+      if (isTopOrBottom) {
+        ballTrait.direction.multiply(Vector2(1, -1));
+      }
     }
-    // If we reach this, we bounce off and increase the speed
-    final ballTrait = get<BallTrait>();
     ballTrait.speed += 20;
-    //TODO This is wrong I'm stuiped - please fix me
-    ballTrait.direction
-        .reflect((intersectionPoints.first - ballTrait.direction).normalized());
   }
 }
